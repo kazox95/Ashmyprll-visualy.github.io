513a8cd9eaeff4478ef30f2c096a117081c7b073
kazox95 <127447362+kazox95@users.noreply.github.com>
2023-12-25 12:50:40 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,43 @@
+
+var credentials = [
+    { username: 'utilisateur1', password: 'motdepasse1' },
+    { username: 'utilisateur2', password: 'motdepasse2' },
+    { username: 'salut', password: 'salut' },
+  { username: 'Gsv238', password: 'Genilson238' },
+    { username: 'Shadowdark', password: 'Shadow28@28@' },
+    { username: 'Moi', password: 'Toi' },
+    { username: 'Kyliandu95', password: '2b_190' },
+    { username: 'Tgr95g', password: '359359Tt-' },
+  ];
+  
+  function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+   
+    var isValidCredentials = credentials.some(function(credential) {
+      return credential.username === username && credential.password === password;
+    });
+  
+    if (isValidCredentials) {
+      ('Connexion rÃ©ussie');
+     
+      window.location.href = './sécurité+.html';
+    } else {
+      alert('Nom d\'utilisateur ou mot de passe incorrect');
+    }
+  }
+  
+
+  document.getElementById('username').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      login(); 
+    }
+  });
+  
+  document.getElementById('password').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      login(); 
+    }
+  });
+
