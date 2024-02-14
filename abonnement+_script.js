03639a28910dcaa993a8f5a50dcbd900698af8df
kazox95 <127447362+kazox95@users.noreply.github.com>
2024-02-14 20:09:13 +0100

    Update abonnement+_script.js

--- abonnement+_script.js
+++ abonnement+_script.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     let orderNumber = localStorage.getItem('orderNumber');
     let orderTotal = localStorage.getItem('orderTotal');
-    let listCart = [];
 
-    document.getElementById('orderNumber').innerText = 'Numéro de Commande : ' + orderNumber;
-    document.getElementById('orderTotal').innerText = 'Prix Total : ' + orderTotal + '€';
+    var orderNumberElement = document.getElementById('orderNumber');
+    var orderTotalElement = document.getElementById('orderTotal');
 
-   
-    addCartToHTML();
+    if (orderNumberElement && orderTotalElement) {
+        orderNumberElement.innerText = 'Numéro de Commande : ' + orderNumber;
+        orderTotalElement.innerText = 'Prix Total : ' + orderTotal + '€';
+
+        addCartToHTML();
+    } else {
+        console.error('Les éléments avec les IDs orderNumber et orderTotal n\'ont pas été trouvés.');
+    }
 });
 
 let listCart = [];
@@ -24,16 +28,13 @@ function checkCart() {
 
 checkCart();
 addCartToHTML();
+
 function addCartToHTML() {
-  
     let listCartHTML = document.getElementById('listCart');
 
-   
     if (listCartHTML) {
-     
         listCartHTML.innerHTML = '';
 
-      
         let totalQuantityHTML = document.querySelector('.totalQuantity');
         let totalPriceHTML = document.querySelector('.totalPrice');
         let totalQuantity = 0;
@@ -43,7 +44,7 @@ function addCartToHTML() {
             listCart.forEach(product => {
                 if (product) {
                     let newCartItem = document.createElement('div');
-                    newCartItem.classList.add('cart-item'); 
+                    newCartItem.classList.add('cart-item');
                     newCartItem.innerHTML = `
                         <div class="item-info">
                             <img src="${product.image}" alt="${product.name}" style="max-width: 100px; max-height: 100px;">
@@ -53,13 +54,46 @@ function addCartToHTML() {
                         </div>
                     `;
                     listCartHTML.appendChild(newCartItem);
+
+                    // Mettre à jour les totaux
+                    totalQuantity += product.quantity;
+                    totalPrice += parseFloat(product.actual_price) * product.quantity;
                 }
             });
         }
+
+        totalQuantityHTML.innerText = totalQuantity;
+
+        // Assurez-vous que totalPrice est calculé correctement
+        totalPrice = parseFloat(totalPrice.toFixed(2));
+
+        if (!isNaN(totalPrice)) {
+            totalPriceHTML.innerText = totalPrice.toFixed(2) + '€';
+        } else {
+            totalPriceHTML.innerText = '$0.00';
+        }
     } else {
-        console.log('L\'élément avec la classe "list" n\'a pas été trouvé.');
+        console.log('L\'élément avec l\'ID "listCart" n\'a pas été trouvé.');
     }
 }
+
+// Reste de votre script...
+
+// Récupérer les informations du client depuis le localStorage
+var customerInfo = JSON.parse(localStorage.getItem('customerInfo'));
+
+// Vérifier si les informations existent
+if (customerInfo) {
+    // Afficher les informations où vous le souhaitez dans votre page
+    document.getElementById('customerName').innerText = customerInfo.name;
+    document.getElementById('customerPhone').innerText = customerInfo.phone;
+    document.getElementById('customerAddress').innerText = customerInfo.address;
+    document.getElementById('customerCountry').innerText = customerInfo.country;
+}
+
+
+
+
 
 
 function generatePDF() {
@@ -69,37 +103,51 @@ function generatePDF() {
         var orderNumber = document.getElementById('orderNumber').innerText;
         var orderTotal = document.getElementById('orderTotal').innerText;
 
+        // Récupérer les informations du client depuis le localStorage
+        var customerInfo = JSON.parse(localStorage.getItem('customerInfo'));
+
+        // Informations du client stylisées dans un encadré
+        var customerInfoBox = `
+            <div style="border: 1px solid #ddd; border-radius: 10px; padding: 10px; background-color: #f9f9f9; margin-bottom: 20px;">
+                <strong>Informations du Client</strong><br>
+                Nom : ${customerInfo.name}<br>
+                Téléphone : ${customerInfo.phone}<br>
+                Adresse Email : ${customerInfo.address}<br>
+                Pays : ${customerInfo.country}
+            </div>
+        `;
+
         // Informations importantes stylisées dans un encadré
         var importantInfo = `
             <div style="text-align: center; margin-top: 20px;">
                 <h3>Informations importantes</h3>
             </div>
-            
-              
+
             <div style="border: 1px solid #ddd; border-radius: 10px; padding: 10px; background-color: #f9f9f9; margin: 10px;">     
-                    <strong>Cette facture vous est fournie une seule fois. En cas de problème ou de plainte au Service Après-Vente (SAV), veuillez vous référer au numéro de facture indiqué ci-dessus.</strong>
-                </div>
+                <strong>Cette facture vous est fournie une seule fois. En cas de problème ou de plainte au Service Après-Vente (SAV), veuillez vous référer au numéro de facture indiqué ci-dessus.</strong>
+              <br>
+                <strong>Aucun remboursement ne sera envisageable après l'achat.</strong>
             </div>
         `;
 
         // Logo et informations personnelles
         var logoAndInfo = `
             <div style="text-align: center; margin-top: 20px;">
-                <img src="./LOGOOGOOGO-removebg-preview.png" style="max-width: 225px; max-height: 225px; margin-bottom: 20px;">
+                <img src="./LOGOOGOOGO-removebg-preview.png" style="max-width: 150px; max-height: 150px; margin-bottom: 10px;">
             </div>
-            <div style="text-align: left; font-weight: bold; margin-bottom: 20px;">
+            <div style="text-align: left; font-weight: bold; margin-bottom: 10px;">
                 Responsable: Ashmy_Prl  (Insta ashmy_prl02)
             </div>
         `;
 
         // Génération du contenu du PDF
-        var pdfContent = document.getElementById('orderDetails').outerHTML;
+        var pdfContent = customerInfoBox + document.getElementById('orderDetails').outerHTML;
 
         var items = document.querySelectorAll('.returnCart .list .item');
         items.forEach(item => {
-            // Produits stylisés dans un encadré
+            // Produits stylisés dans un encadré avec une taille réduite
             var styledItem = `
-                <div style="border: 1px solid #ddd; border-radius: 10px; padding: 10px; margin-bottom: 10px; background-color: #f9f9f9;">
+                <div style="border: 1px solid #ddd; border-radius: 10px; padding: 5px; margin-bottom: 5px; background-color: #f9f9f9;">
                     ${item.outerHTML}
                 </div>
             `;
@@ -112,19 +160,24 @@ function generatePDF() {
         // Ajout du logo et des informations personnelles
         pdfContent = logoAndInfo + pdfContent;
 
+        // Ajout de la date et de la signature
+        var currentDate = new Date().toLocaleString();
+        var signature = `
+            <div style="text-align: right; margin-top: 20px;">
+                <p>Date et heure de génération : ${currentDate}</p>
+                <img src="./Signature.png" style="max-width: 1500px; max-height: 5000px;">
+            </div>
+        `;
+        pdfContent += signature;
+
         // Création de l'élément div pour le contenu PDF
         var element = document.createElement('div');
         element.innerHTML = pdfContent;
 
-        // Suppression des boutons de la page du PDF
-        element.querySelectorAll('button').forEach(button => {
-            button.remove();
-        });
-
         // Options de génération PDF
         var opt = {
             margin: 0.5,
-            filename: `Facture de C.M.G_VISUALY.pdf`,
+            filename: `Facture_de_C.M.G_VISUALY.pdf`,
             image: { type: 'jpeg', quality: 1 },
             html2canvas: { scale: 2 },
             jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
